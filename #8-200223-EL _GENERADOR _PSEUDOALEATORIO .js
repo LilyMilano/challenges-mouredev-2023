a04cfb33ac5e10f86fbd54c481e6da5b0c89fd90
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-03-14 01:22:47 -0400

    use strict

--- #8-200223-EL _GENERADOR _PSEUDOALEATORIO .js
+++ #8-200223-EL _GENERADOR _PSEUDOALEATORIO .js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
  * Crea un generador de números pseudoaleatorios entre 0 y 100.
  * - No puedes usar ninguna función "random" (o semejante) del lenguaje de programación seleccionado.
