8e591bcd98c03ca17d395ef13873b3c1fe8536de
Liliana Milano <107228065+LilyMilano@users.noreply.github.com>
2023-05-03 20:48:01 -0400

    Solve challenge #16: draw a staircase

---
+++ #16-170423-LA_ESCALERA.js
@@ -0,0 +1,54 @@
+/*
+ * Crea una función que dibuje una escalera según su número de escalones.
+ * - Si el número es positivo, será ascendente de izquiera a derecha.
+ * - Si el número es negativo, será descendente de izquiera a derecha.
+ * - Si el número es cero, se dibujarán dos guiones bajos (__).
+ *
+ * Ejemplo: 4
+ *         _
+ *       _|
+ *     _|
+ *   _|
+ * _|
+ *
+ */
+
+function drawStaircase(steps) {
+	const resultParts = [];
+
+	switch (true) {
+		case steps === 0:
+			resultParts.push(`__`);
+			break;
+		case steps > 0:
+			resultParts.push(`${' '.repeat(steps)}_\n`);
+			for (let i = 0; i < steps; i++) {
+				resultParts.push(`${' '.repeat(steps - i - 1)}_|\n`);
+			}
+			break;
+		case steps < 0:
+			for (let i = 0; i < Math.abs(steps); i++) {
+				resultParts.push(`${' '.repeat(i)}|_\n`);
+			}
+			resultParts.push(`${' '.repeat(Math.abs(steps))}|`);
+			break;
+	}
+	return resultParts.join('');
+}
+
+// Testing:
+
+console.log(drawStaircase(0));
+// Log: __
+console.log(drawStaircase(4));
+// Log: _
+//     _|
+//    _|
+//   _|
+//  _|
+console.log(drawStaircase(-4));
+// Log: |_
+//       |_
+//        |_
+//         |_
+//          |
